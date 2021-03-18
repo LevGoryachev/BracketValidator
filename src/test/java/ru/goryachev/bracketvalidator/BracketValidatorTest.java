bef6196f249dea67f706ad0b5daceb2803b5e0e3
Lev <l.goryachev@inbox.ru>
2021-03-18 12:31:00 +0300

    add lib JUnitJupiter5, add parameterized tests

--- src/test/java/ru/goryachev/bracketvalidator/BracketValidatorTest.java
+++ src/test/java/ru/goryachev/bracketvalidator/BracketValidatorTest.java
@@ -1,15 +1,24 @@
 package ru.goryachev.bracketvalidator;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BracketValidatorTest {
 
-    @Test
-    public void checkBrackets() {
-        BracketValidator bracketValidator = new BracketValidator("abc(def)gh");
-        boolean result = bracketValidator.validate();
-        assertTrue(result);
+    @ParameterizedTest
+    //Lev Goryachev: set first parameter correct, second parameter incorrect
+    @CsvSource({"'abc(def)gh', 'abcdef)gh'", "'xyz{l}bc', 'xyz{lb[]c'", "'abc[def]gh', 'abc[de}fgh'", "'abc(d[]ef)g{}h', 'abc(defg])h'", "'x[yz{l()}b]c', 'x{y{z}lb()c'"})
+    public void checkBrackets(String correct, String incorrect) {
+        BracketValidator correctValidator = new BracketValidator(correct);
+        boolean corResult = correctValidator.validate();
+        assertTrue(corResult);
+
+        BracketValidator incorrectValidator = new BracketValidator(incorrect);
+        boolean incorResult = incorrectValidator.validate();
+        assertFalse(incorResult);
     }
 }
