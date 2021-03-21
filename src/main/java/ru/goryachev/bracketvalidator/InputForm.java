4d59b87cc3e9ce3ae982d3c9d3a75dc73318c167
Lev <l.goryachev@inbox.ru>
2021-03-21 16:29:00 +0300

    InputForm: checking of null and empty String

--- src/main/java/ru/goryachev/bracketvalidator/InputForm.java
+++ src/main/java/ru/goryachev/bracketvalidator/InputForm.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class InputForm {
 
+    String checkingString = null;
+
     public String getCheckingString () {
         System.out.println("Введите текст для проверки:");
         Scanner console = new Scanner(System.in);
-        return console.nextLine();
+        this.checkingString = console.nextLine();
+        while (checkingString == null || checkingString.equals("") || checkingString.equals(" ")) {
+            System.out.println("Текст для проверки отсутствует. Попробуйте ещё раз.");
+            this.checkingString = console.nextLine();
+        }
+        return checkingString;
     }
 }
